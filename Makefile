6ff5d64b0a3b69e87780f79b2ef4971028ed46e1
flaribbit <1149761294@qq.com>
2019-12-03 15:07:17 +0800

    完成两个函数

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CFLAGS=-I ./include -s -O2 -DNDEBUG
 
 cold_clear_wrapper.dll: cold_clear_wrapper.c
-	gcc $(CFLAGS) -shared cold_clear_wrapper.c lua51.dll -o cold_clear_wrapper.dll
+	gcc $(CFLAGS) -shared cold_clear_wrapper.c cold_clear.dll lua51.dll -o cold_clear_wrapper.dll
--- cold_clear_wrapper.c
+++ cold_clear_wrapper.c
@@ -1,20 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "coldclear.h"
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 
-static int About(lua_State *L){
-    printf("wrapper by flaribbit\n");
+//CCAsyncBot *cc_launch_async(CCOptions *options, CCWeights *weights);
+static int launch_async(lua_State *L){
+    CCOptions *options=(CCOptions*)lua_tointeger(L,1);
+    CCWeights *weights=(CCWeights*)lua_tointeger(L,2);
+    CCAsyncBot *bot=cc_launch_async(options,weights);
+    lua_pushinteger(L,(lua_Integer)bot);
+    return 1;
+}
+
+//void cc_destroy_async(CCAsyncBot *bot);
+static int destroy_async(lua_State *L){
+    CCAsyncBot *bot=(CCAsyncBot*)lua_tointeger(L,1);
+    cc_destroy_async(bot);
     return 0;
+}
+
+static int about(lua_State *L){
+    lua_pushstring(L,"wrapper by flaribbit");
+    return 1;
 }
 
 static const struct luaL_Reg funcList[]=
 {
-    {"About", About},
+    {"about", about},
+    {"launch_async",launch_async},
     {0, 0}
 };
 
-int luaopen_testlibs_libtt(lua_State *L)
+int luaopen_cold_clear_wrapper(lua_State *L)
 {
     luaL_register(L, "cc", funcList);
     return 1;
