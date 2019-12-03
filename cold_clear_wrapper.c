d3fb82c02b2e3226ad4f820192059348b8795f82
flaribbit <1149761294@qq.com>
2019-12-03 15:54:03 +0800

    三个函数完成

--- cold_clear_wrapper.c
+++ cold_clear_wrapper.c
@@ -21,6 +21,21 @@ static int destroy_async(lua_State *L){
     return 0;
 }
 
+//void cc_reset_async(CCAsyncBot *bot, bool *field, bool b2b, uint32_t combo);
+static int reset_async(lua_State *L){
+    CCAsyncBot *bot=(CCAsyncBot*)lua_tointeger(L,1);
+    bool b2b=lua_toboolean(L,3);
+    int combo=lua_tointeger(L,4);
+    bool field[400];
+    int i=0;
+    while(lua_next(L,2)){
+        field[i++]=lua_toboolean(L,-1);
+        lua_pop(L,1);
+    }
+    cc_reset_async(bot,field,b2b,combo);
+    return 0;
+}
+
 static int about(lua_State *L){
     lua_pushstring(L,"wrapper by flaribbit");
     return 1;
@@ -30,6 +45,8 @@ static const struct luaL_Reg funcList[]=
 {
     {"about", about},
     {"launch_async",launch_async},
+    {"destroy_async",destroy_async},
+    {"reset_async",reset_async},
     {0, 0}
 };
 
