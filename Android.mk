a9437b4119b8aff9224cc8e922f4e93140c23bed
flaribbit <1149761294@qq.com>
2020-10-14 12:36:50 +0800

    fix linking issue

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := CCloader
 LOCAL_SRC_FILES := cold_clear_wrapper.c
-LOCAL_LDFLAGS := -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -lcold_clear -llove
+LOCAL_LDFLAGS := -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -lcold_clear -llove -Wl,--hash-style=both
 LOCAL_C_INCLUDES := include cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
