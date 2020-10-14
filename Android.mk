7abb7e47f38c73d9cc93f878a0b25a54d1ca407b
flaribbit <1149761294@qq.com>
2020-10-14 11:20:47 +0800

    fix include path

--- Android.mk
+++ Android.mk
@@ -3,5 +3,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := CCloader
 LOCAL_SRC_FILES := cold_clear_wrapper.c
 LOCAL_LDFLAGS := -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -lcold_clear -llove
-LOCAL_C_INCLUDES := cold-clear/c-api
+LOCAL_C_INCLUDES := include cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
