69cd5b550ec0a22331c3e2cfc1ad3eb36d0546cb
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-09-05 16:32:55 +0800

    fix android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := CCloader
-LOCAL_SRC_FILES := cold_clear_wrapper.c $(LOVE2D_LIB)/$(TARGET_ARCH_ABI)/libcoldclear.a
-LOCAL_LDFLAGS := -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -llove -Wl,--hash-style=both
+LOCAL_SRC_FILES := cold_clear_wrapper.c
+LOCAL_LDFLAGS := -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -Wl,--hash-style=both
+LOCAL_STATIC_LIBRARIES := cold_clear
+LOCAL_SHARED_LIBRARIES := love
 LOCAL_C_INCLUDES := include cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
