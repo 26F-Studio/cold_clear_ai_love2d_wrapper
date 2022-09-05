dc9038530e88e3d3060806bd1ee0aec82c1abb31
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-09-05 20:53:00 +0800

    remove dependence on libstdc++.so

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := CCloader
 LOCAL_SRC_FILES := cold_clear_wrapper.c
-LOCAL_LDFLAGS := $(LOVE2D_LIB)/$(TARGET_ARCH_ABI)/libcold_clear.a -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -llove -Wl,--hash-style=both
+LOCAL_LDFLAGS := $(LOVE2D_LIB)/$(TARGET_ARCH_ABI)/libcold_clear.a -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -llove -lc++_shared -Wl,--hash-style=both
 LOCAL_C_INCLUDES := include cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
