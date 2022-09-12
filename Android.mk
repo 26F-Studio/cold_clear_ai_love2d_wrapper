7eef4fc11b282d47b57d59ef626179fa85badbdd
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-09-13 03:02:25 +0800

    use fake object to build libcold_clear.a

--- Android.mk
+++ Android.mk
@@ -2,14 +2,16 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := CCloader
 LOCAL_MODULE_FILENAME := CCloader
+CCNAME := cold_clear-$(TARGET_ARCH_ABI)
 
-$(LOCAL_PATH)/libcold_clear.a:
+$(LOCAL_PATH)/$(CCNAME).c
 	cd cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform 24 build -p c-api --release
-	cp cold-clear/target/*/$(@F) $@
+	cp cold-clear/target/*/libcold_clear.a lib$(CCNAME).a
 	cd cold-clear && cargo clean
-.INTERMEDIATE: $(LOCAL_PATH)/libcold_clear.a
+	touch $@
+.PHONY: $(LOCAL_PATH)/$(CCNAME).c
 
-LOCAL_SRC_FILES := cold_clear_wrapper.c libcold_clear.a
-LOCAL_LDFLAGS := -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -llove -Wl,--hash-style=both
+LOCAL_SRC_FILES := cold_clear_wrapper.c $(CCNAME).c
+LOCAL_LDFLAGS := -l$(CCNAME) -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -llove -Wl,--hash-style=both
 LOCAL_C_INCLUDES := include cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
