c4cde81de6e4b7ca602794529b26a02b71d49e86
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-09-13 02:49:42 +0800

    fix libcold_clear.a

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := CCloader
 LOCAL_MODULE_FILENAME := CCloader
-LOCAL_SRC_FILES := cold_clear_wrapper.c libcold_clear.a
 
 $(LOCAL_PATH)/libcold_clear.a:
 	cd cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform 24 build -p c-api --release
@@ -10,6 +9,7 @@ $(LOCAL_PATH)/libcold_clear.a:
 	cd cold-clear && cargo clean
 .INTERMEDIATE: $(LOCAL_PATH)/libcold_clear.a
 
+LOCAL_SRC_FILES := cold_clear_wrapper.c libcold_clear.a
 LOCAL_LDFLAGS := -L$(LOVE2D_LIB)/$(TARGET_ARCH_ABI) -llove -Wl,--hash-style=both
 LOCAL_C_INCLUDES := include cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
