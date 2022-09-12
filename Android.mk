12893dafb17af48daae5c0d189a5ef063dfd9f79
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-09-13 05:25:25 +0800

    replace some LOCAL_PATHs with shorter names

--- Android.mk
+++ Android.mk
@@ -11,13 +11,13 @@ $(MY_COLDCLEAR_LOCAL_PATH)/$(CCNAME).c:
 		arm64-v8a) rustup target add aarch64-linux-android;; \
 		*) exit 1;; \
 	esac
-	cd $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform 24 build -p c-api --release
-	cp $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear/target/*/release/libcold_clear.a $(MY_COLDCLEAR_LOCAL_PATH)/lib$(CCNAME).a
-	cd $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear && cargo clean
+	cd $(@D)/cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform 24 build -p c-api --release
+	cp $(@D)/cold-clear/target/*/release/libcold_clear.a $(@D)/lib$(CCNAME).a
+	cd $(@D)/cold-clear && cargo clean
 	touch $@
 .PHONY: $(MY_COLDCLEAR_LOCAL_PATH)/$(CCNAME).c
 
 LOCAL_SRC_FILES := cold_clear_wrapper.c $(CCNAME).c
-LOCAL_LDFLAGS := lib$(CCNAME).a -llove -Wl,--hash-style=both
+LOCAL_LDFLAGS := $(MY_COLDCLEAR_LOCAL_PATH)/lib$(CCNAME).a -llove -Wl,--hash-style=both
 LOCAL_C_INCLUDES := $(MY_COLDCLEAR_LOCAL_PATH)/include $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
