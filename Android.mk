5df700b6a8272d4be0c2dc49223377f6d53b8d4d
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-09-13 05:22:51 +0800

    replace all LOCAL_PATH with custom name

--- Android.mk
+++ Android.mk
@@ -1,24 +1,23 @@
-LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+MY_COLDCLEAR_LOCAL_PATH := $(call my-dir)
 LOCAL_MODULE := CCloader
 LOCAL_MODULE_FILENAME := CCloader
 CCNAME := cold_clear-$(TARGET_ARCH_ABI)
-MY_CC_LOCAL_PATH := $(LOCAL_PATH)
 
-$(MY_CC_LOCAL_PATH)/$(CCNAME).c:
+$(MY_COLDCLEAR_LOCAL_PATH)/$(CCNAME).c:
 	cargo install cargo-ndk
 	case $(TARGET_ARCH_ABI) in \
 		armeabi-v7a) rustup target add armv7-linux-androideabi;; \
 		arm64-v8a) rustup target add aarch64-linux-android;; \
 		*) exit 1;; \
 	esac
-	cd $(MY_CC_LOCAL_PATH)/cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform 24 build -p c-api --release
-	cp $(MY_CC_LOCAL_PATH)/cold-clear/target/*/release/libcold_clear.a $(MY_CC_LOCAL_PATH)/lib$(CCNAME).a
-	cd $(MY_CC_LOCAL_PATH)/cold-clear && cargo clean
+	cd $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform 24 build -p c-api --release
+	cp $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear/target/*/release/libcold_clear.a $(MY_COLDCLEAR_LOCAL_PATH)/lib$(CCNAME).a
+	cd $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear && cargo clean
 	touch $@
-.PHONY: $(MY_CC_LOCAL_PATH)/$(CCNAME).c
+.PHONY: $(MY_COLDCLEAR_LOCAL_PATH)/$(CCNAME).c
 
 LOCAL_SRC_FILES := cold_clear_wrapper.c $(CCNAME).c
 LOCAL_LDFLAGS := lib$(CCNAME).a -llove -Wl,--hash-style=both
-LOCAL_C_INCLUDES := $(MY_CC_LOCAL_PATH)/include $(MY_CC_LOCAL_PATH)/cold-clear/c-api
+LOCAL_C_INCLUDES := $(MY_COLDCLEAR_LOCAL_PATH)/include $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
