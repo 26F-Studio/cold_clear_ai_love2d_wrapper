ed5b441b422ff0c1f0d14a686aec018ee39de4d0
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-09-13 19:38:23 +0800

    move application.mk into makefile

--- Android.mk
+++ Android.mk
@@ -13,7 +13,7 @@ $(MY_COLDCLEAR_LOCAL_PATH)/$(CCNAME).c:
 		x86_64) rustup target add x86_64-linux-android;; \
 		*) echo unsupported platform $(TARGET_ARCH_ABI); exit 1;; \
 	esac
-	cd $(@D)/cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform 24 build -p c-api --release
+	cd $(@D)/cold-clear && cargo ndk -t $(TARGET_ARCH_ABI) --platform `sed s/android-//g <<< $(TARGET_PLATFORM)` build -p c-api --release
 	cp $(@D)/cold-clear/target/*/release/libcold_clear.a $(@D)/lib$(CCNAME).a
 	cd $(@D)/cold-clear && cargo clean
 	touch $@
@@ -21,8 +21,11 @@ $(MY_COLDCLEAR_LOCAL_PATH)/$(CCNAME).c:
 
 LOCAL_SRC_FILES := cold_clear_wrapper.c $(CCNAME).c
 LOCAL_LDFLAGS := $(MY_COLDCLEAR_LOCAL_PATH)/lib$(CCNAME).a -Wl,--hash-style=both
+LOCAL_C_INCLUDES := $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear/c-api
 ifneq ($(MY_COLDCLEAR_STANDALONE),yes)
 	LOCAL_SHARED_LIBRARIES := liblove
+else
+	LOCAL_LDFLAGS += -L$(LOVE_HOME)/lib/$* -llove
+	LOCAL_C_INCLUDES += $(MY_COLDCLEAR_STANDALONE)/include
 endif
-LOCAL_C_INCLUDES := $(MY_COLDCLEAR_LOCAL_PATH)/cold-clear/c-api
 include $(BUILD_SHARED_LIBRARY)
--- Application.mk
+++
@@ -1,6 +0,0 @@
-APP_CPPFLAGS := -frtti 
-APP_LDFLAGS := -latomic
-APP_ABI := $(APP_ABI)
-APP_PLATFORM := android-28
-APP_OPTIM := release
-APP_STL := none
